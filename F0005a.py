ea6ecf812ad3f3ae6e911df5ec9ca771918ee8a1
PatrickNastase1 <patricknastase13@gmail.com>
2020-11-25 07:41:42 +0000

    Initial commit

---
+++ F0005a.py
@@ -0,0 +1,4 @@
+A kod bekér egy jelszót, és ha a  jelszó helytelen,
+kiírja a képernyőre azt hogy 'hozzáférés megtagadva',
+de ha jó a jelszó akkor meg azt írja hogy 'bemehet'!
+(Mivel tanárúr nem válaszolt messengeren .ezért Tomi-val ez volt az elképzelésünk erről a  feladatról ,hogy így kell...)
---
+++ F0005b.py
@@ -0,0 +1,6 @@
+a=input('kérem a jelszót= ')
+jelszo='asd'
+if jelszo == a:
+    print('bemehet')
+else:
+    print('hozzáférés megtagadva')
